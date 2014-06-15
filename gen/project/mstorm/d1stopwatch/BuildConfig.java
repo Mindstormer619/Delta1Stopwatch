b912d6fef552c73192b76ec0f77f5482f20ccace
Siddarth Iyer <mindstormer619@gmail.com>
2014-06-15 22:47:37 +0400

    Commit 1: Still at work
    Work in progress. please.

---
+++ gen/project/mstorm/d1stopwatch/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package project.mstorm.d1stopwatch;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/project/mstorm/d1stopwatch/MainActivity.java
@@ -0,0 +1,60 @@
+package project.mstorm.d1stopwatch;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.widget.TextView;
+import android.app.Activity;
+
+public class MainActivity extends Activity {
+	TextView displayer;
+
+    Handler msgHandler = new Handler() {
+    	public void handleMessage(Message msg) {
+    		Bundle tBund = msg.getData();
+    		String tCurr = String.valueOf(tBund.getInt("time"));
+    		displayer.setText(tCurr);
+    	}
+    };
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        displayer = (TextView) findViewById(R.id.textView1);
+    }
+    
+    @Override
+    protected void onStart() {
+    	// TODO Auto-generated method stub
+    	super.onStart();
+    	Thread backUpdate = new Thread( new Runnable() {
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				int tPass=0;
+				while(true) {
+					if (Thread.interrupted()) {
+						return;
+					}
+					try {
+						Thread.sleep(1000);
+						if (Thread.interrupted()) {
+							return;
+						}
+						Bundle b = new Bundle();
+						tPass++;
+						b.putInt("time", tPass);
+						Message msg = new Message();
+						msg.setData(b);
+						msgHandler.sendMessage(msg);
+					}
+					catch(Exception e) {
+						return;
+					}
+				}
+			}
+		});
+    	backUpdate.start();
+    }
+}
